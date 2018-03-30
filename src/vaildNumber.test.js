78c10872df8c67cb6ad76d83c1335f3a59ab7830
Porco home <zhangporco@gmail.com>
2018-03-30 14:39:14 +0800

    vaild number

--- src/vaildNumber.test.js
+++ src/vaildNumber.test.js
@@ -1,7 +1,40 @@
 const test = require('ava');
 
+const isNumber = function(s) {
+    s = s.replace(/^\s+|\s+$/g,"");
+    if (!s) return false; 
+    var val = +s;
+    if (val || val === 0) {
+        return true;
+    }
+    return false;
+}
 
-test('vaild number', t => {
-    
-    t.pass();
+test('vaild number1', t => {
+    const res = isNumber('0');
+    t.true(res);
+});
+test('vaild number2', t => {
+    const res = isNumber(' 0.1 ');
+    t.true(res);
+});
+test('vaild number3', t => {
+    const res = isNumber('abc');
+    t.false(res);
+});
+test('vaild number4', t => {
+    const res = isNumber('1 a');
+    t.false(res);
+});
+test('vaild number5', t => {
+    const res = isNumber('2e10');
+    t.true(res);
+});
+test('vaild number6', t => {
+    const res = isNumber(' ');
+    t.false(res);
+});
+test('vaild number7', t => {
+    const res = isNumber(". 1");
+    t.false(res);
 });
